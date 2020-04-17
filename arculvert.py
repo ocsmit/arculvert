e50b58a8b7990694c12dbab4fc3962267d2898a5
ocsmit <owen8461@protonmail.com>
2020-04-17 15:27:01 -0400

    Create process_dem
    Remove gdb processing, transition to output folder. More versitile this way

--- arculvert.py
+++ arculvert.py
@@ -7,11 +7,23 @@
 import arcpy
 import os
 
-def create_GDB(out_folder, gdb_name):
-    arcpy.CreateFileGDB_management(out_folder, gdb_name)
-    if not gdb_name.endswith('.gdb'):
-        gdb = '%s%s' % (gdb_name, '.gdb')
-    else:
-        gdb = gdb_name
-    gdb_path = os.path.join(os.path.abspath(out_folder), gdb)
-    return gdb_path
+
+def process_dem(out_folder, dem_path, mask=None,
+                masked_raster_name='masked_dem'):
+    raster = arcpy.Raster(dem_path)
+    if mask:
+        if not masked_raster_name.endswith('.tif'):
+            out_path = '%s/%s%s' % (out_folder, masked_raster_name, '.tif')
+            out_mask = arcpy.sa.ExtractByMask(raster, mask)
+            dem = out_mask.save(out_path)
+        else:
+            out_mask = arcpy.sa.ExtractByMask(raster, mask)
+            out_path = '%s/%s' % (out_folder, masked_raster_name)
+            dem = out_mask.save(out_path)
+    if not mask:
+        out_path = '%s/%s' % (out_folder, os.path.basename(dem_path))
+        dem = raster.save(out_path)
+
+    return dem
+
+
