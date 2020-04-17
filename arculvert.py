0b5e2ac3369d37034b5a599b35e086e407828541
ocsmit <owen8461@protonmail.com>
2020-04-17 13:24:59 -0400

    Add GDB creation
    Function to create GDB where all data will be output.

--- arculvert.py
+++ arculvert.py
@@ -3,3 +3,15 @@
 # Purpose: A Python module for ArcGIS Pro designed to detect culverts
 # Author: Owen Smith
 ################################################################################
+
+import arcpy
+import os
+
+def create_GDB(out_folder, gdb_name):
+    arcpy.CreateFileGDB_management(out_folder, gdb_name)
+    if not gdb_name.endswith('.gdb'):
+        gdb = '%s%s' % (gdb_name, '.gdb')
+    else:
+        gdb = gdb_name
+    gdb_path = os.path.join(out_folder, gdb)
+    return gdb_path
